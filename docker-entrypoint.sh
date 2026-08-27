18416b3ce70c9b7c84dda32492752beafa81d216
alperates58 <alperates58@gmail.com>
2026-08-27 21:12:33 +0300

    fix(deploy): harden postgres startup healthcheck for coolify

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 set -e
 
-echo "[Filmprint Entrypoint] Running Prisma database migrations..."
-./node_modules/.bin/prisma migrate deploy
-
 echo "[Filmprint Entrypoint] Starting Next.js standalone server..."
 exec node server.js
